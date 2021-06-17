9f8ac65c6f0b0354b40bf2adaba616c92a1a55b3
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-06-17 22:16:18 +0200

    feat : Modal for art images

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,8 +65,10 @@ bloopBtn.onclick = function() {
 
 }
 
-// saving input value even if refreshed
 
+
+
+// saving input value even if refreshed
 var inputBox = document.getElementById("textInput")
 
 // If ze qlreqdy hqve a value, retreive it
@@ -89,6 +91,55 @@ inputBox.addEventListener("keyup", function() {
     console.log(inputBox.value)
 
 })
+
+
+
+
+
+
+
+
+
+
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "flex";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+}
 
 
 
